8acae7700b2d8e89e83852c272184ec26772f481
Rohan Shah <moreinmore@gmail.com>
2025-07-15 12:57:00 +0530

    Revert "Feat: Initialize node"

--- public/index.js
+++
@@ -1,6 +0,0 @@
-import { renderNode } from "./helpers/render.js";
-import { virtualDOM } from "./constants/vdom.js";
-
-const root = document.getElementById("root")
-const renderedDom = renderNode(virtualDOM)
-root.appendChild(renderedDom)
